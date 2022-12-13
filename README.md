bdaa5c9a8da341efaf5df0ffd997f51c25f25b66
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2022-12-13 17:21:37 +0100

    format by shfmt

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 # cups-alpine
 
+[![build][badge-gh-actions-build]][link-gh-actions-build]
+
 Fork from [chuckcharlie/docker-cups-airprint](https://github.com/chuckcharlie/cups-avahi-airprint)
 
 ## Configuration
 
-### Volumes:
+### Volumes
+
 * `/config`: where the persistent printer configs will be stored
 * `/services`: where the Avahi service files will be generated
 
-### Variables:
+### Variables
+
 * `CUPSADMIN`: the CUPS admin user you want created - default is CUPSADMIN if unspecified
 * `CUPSPASSWORD`: the password for the CUPS admin user - default is admin username if unspecified
 
-### Ports/Network:
+### Ports/Network
+
 * Must be run on host network. This is required to support multicasting which is needed for Airprint.
 
-### Example run command:
-```
+### Example run command
+
+```sh
 docker run --name cups --restart unless-stopped  --net host\
   -v <your services dir>:/services \
   -v <your config dir>:/config \
@@ -25,8 +31,11 @@ docker run --name cups --restart unless-stopped  --net host\
   chuckcharlie/cups-avahi-airprint:latest
 ```
 
-## Add and set up printer:
+## Add and set up printer
+
 * CUPS will be configurable at http://[host ip]:631 using the CUPSADMIN/CUPSPASSWORD.
 * Make sure you select `Share This Printer` when configuring the printer in CUPS.
 * ***After configuring your printer, you need to close the web browser for at least 60 seconds. CUPS will not write the config files until it detects the connection is closed for as long as a minute.***
 
+[badge-gh-actions-build]: https://github.com/g3rhard/docker-cups-alpine/actions/workflows/build.yml/badge.svg?branch=production
+[link-gh-actions-build]: https://github.com/g3rhard/docker-cups-alpine/actions?query=workflow%3Abuild
--- root/root/airprint-generate.py
+++ root/root/airprint-generate.py
@@ -109,10 +109,10 @@ class AirPrintGenerate(object):
         self.directory = directory
         self.prefix = prefix
         self.adminurl = adminurl
-        
+
         if self.user:
             cups.setUser(self.user)
-    
+
     def generate(self):
         if not self.host:
             conn = cups.Connection()
@@ -120,9 +120,9 @@ class AirPrintGenerate(object):
             if not self.port:
                 self.port = 631
             conn = cups.Connection(self.host, self.port)
-            
+
         printers = conn.getPrinters()
-        
+
         for p, v in list(printers.items()):
             if v['printer-is-shared']:
                 attrs = conn.getPrinterAttributes(p)
@@ -150,17 +150,17 @@ class AirPrintGenerate(object):
                   rp = uri.path
                 else:
                   rp = uri[2]
-                
+
                 re_match = re.match(r'^//(.*):(\d+)(/.*)', rp)
                 if re_match:
                   rp = re_match.group(3)
-                
+
                 #Remove leading slashes from path
                 #TODO XXX FIXME I'm worried this will match broken urlparse
                 #results as well (for instance if they don't include a port)
                 #the xml would be malform'd either way
                 rp = re.sub(r'^/+', '', rp)
-                
+
                 path = Element('txt-record')
                 path.text = 'rp=%s' % (rp)
                 service.append(path)
@@ -224,12 +224,12 @@ class AirPrintGenerate(object):
                     admin = Element('txt-record')
                     admin.text = 'adminurl=%s' % (v['printer-uri-supported'])
                     service.append(admin)
-                
+
                 fname = '%s%s.service' % (self.prefix, p)
-                
+
                 if self.directory:
                     fname = os.path.join(self.directory, fname)
-                
+
                 f = open(fname, 'w')
 
                 if etree:
@@ -241,7 +241,7 @@ class AirPrintGenerate(object):
                     doc.insertBefore(dt, doc.documentElement)
                     doc.writexml(f)
                 f.close()
-                
+
                 if self.verbose:
                     sys.stderr.write('Created: %s%s' % (fname, os.linesep))
 
@@ -264,18 +264,18 @@ if __name__ == '__main__':
         default='AirPrint-')
     parser.add_option('-a', '--admin', action="store_true", dest="adminurl",
         help="Include the printer specified uri as the adminurl")
-    
+
     (options, args) = parser.parse_args()
-    
+
     # TODO XXX FIXME -- if cups login required, need to add
     # air=username,password
     from getpass import getpass
     cups.setPasswordCB(getpass)
-    
+
     if options.directory:
         if not os.path.exists(options.directory):
             os.mkdir(options.directory)
-    
+
     apg = AirPrintGenerate(
         user=options.username,
         host=options.hostname,
@@ -285,5 +285,5 @@ if __name__ == '__main__':
         prefix=options.prefix,
         adminurl=options.adminurl,
     )
-    
+
     apg.generate()
